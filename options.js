b0fd8d97c81d3712fda2d1ae438ed8e120de7e7b
Denis Ushanev <deimosowen@gmail.com>
2023-04-09 18:04:23 +0300

    Add Setting

--- options.js
+++ options.js
@@ -2,6 +2,7 @@ const options = {
     elements: {
         btnPrettifyJson: document.getElementById('prettifyJson'),
         btnSaveJson: document.getElementById('saveJson'),
+        btnSetDefault: document.getElementById('setDefault'),
         optionsJson: document.getElementById('optionsJson'),
         notificationContainer: document.getElementById('notification'),
     },
@@ -12,24 +13,58 @@ const options = {
         options.elements.btnSaveJson.addEventListener('click', async () => {
             await options.saveConfig();
         });
+        options.elements.btnSetDefault.addEventListener('click', async () => {
+            const config = await options.setDefaultConfig();
+            const formattedJSON = JSON.stringify(config, undefined, 4);
+            options.elements.optionsJson.value = formattedJSON;
+        });
         await options.loadConfig();
+        options.help();
     },
     getConfig: async () => {
-        const { config } = await chrome.storage.session.get(["config"]);
+        const { config } = await chrome.storage.local.get(["config"]);
         return config;
     },
     loadConfig: async () => {
-        const config = await options.getConfig();
-        const formattedJSON = JSON.stringify(config, undefined, 4);
-        options.elements.optionsJson.value = formattedJSON;
+        try {
+            let config = await options.getConfig();
+            if (!config) {
+                config = await options.setDefaultConfig();
+            }
+            const formattedJSON = JSON.stringify(config, undefined, 4);
+            options.elements.optionsJson.value = formattedJSON;
+        }
+        catch (err) {
+            options.createNotify('is-danger', 'Invalid JSON structure');
+            console.error(err);
+        }
     },
     saveConfig: async () => {
         if (options.validConfig()) {
             const jsonText = options.elements.optionsJson.value;
             const parsedJSON = JSON.parse(jsonText);
-            await chrome.storage.session.set({ config: parsedJSON });
+            await chrome.storage.local.set({ config: parsedJSON });
             options.createNotify('is-success', 'Data saved successfully');
         }
+    },
+    setDefaultConfig: async () => {
+        const defaultConfig = {
+            "BaseUrl": "https://ci.parcsis.org/",
+            "Repository": {
+                "CasePro": [
+                    {
+                        "BuildType": "CasePro_Pulls_CaseProBuildPullSite",
+                        "Name": "CasePro pull requests"
+                    },
+                    {
+                        "BuildType": "CasePro_Linux_BuildDockerImagesPulls",
+                        "Name": "CasePro pull requests (Linux)"
+                    }
+                ]
+            }
+        };
+        await chrome.storage.local.set({ config: defaultConfig });
+        return defaultConfig;
     },
     validConfig: () => {
         try {
@@ -63,6 +98,45 @@ const options = {
         column.appendChild(notification);
         columns.appendChild(column);
         options.elements.notificationContainer.appendChild(columns);
+    },
+    help: () => {
+        document.addEventListener('DOMContentLoaded', () => {
+            function openModal($el) {
+                $el.classList.add('is-active');
+            }
+
+            function closeModal($el) {
+                $el.classList.remove('is-active');
+            }
+
+            function closeAllModals() {
+                (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+                    closeModal($modal);
+                });
+            }
+
+            (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+                const modal = $trigger.dataset.target;
+                const $target = document.getElementById(modal);
+                $trigger.addEventListener('click', () => {
+                    openModal($target);
+                });
+            });
+
+            (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+                const $target = $close.closest('.modal');
+                $close.addEventListener('click', () => {
+                    closeModal($target);
+                });
+            });
+
+            document.addEventListener('keydown', (event) => {
+                const e = event || window.event;
+                if (e.keyCode === 27) {
+                    closeAllModals();
+                }
+            });
+        });
     }
 };
 
